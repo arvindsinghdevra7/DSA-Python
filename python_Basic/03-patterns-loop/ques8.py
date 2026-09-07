8d44b7c6883a4afd4c3cbce21121b1c795d79594
arvindsinghdevra7 <arvindsinghdevra85@gmail.com>
2026-09-07 23:59:50 +0530

    pattern

---
+++ python_Basic/03-patterns-loop/ques8.py
@@ -0,0 +1,11 @@
+for i in range(0,5):
+    for j in range(5-i,0,-1):
+        print(j,end=' ')
+    print()
+
+
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1     
---
+++ python_Basic/03-patterns-loop/ques9.py
@@ -0,0 +1,22 @@
+"""
+1
+12
+123
+1234
+12345
+
+1234
+123
+12
+1
+"""
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+
+for i in range(4,0,-1):
+     for j in range(1,i+1):
+        print(j,end=' ')
+     print()    
